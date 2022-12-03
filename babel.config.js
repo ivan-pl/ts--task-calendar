375248e2c91dd46f0ce15864501236ebb428c3a9
ivan-pl <pleskaiv@gmail.com>
2022-12-03 18:57:23 +0300

    build: setup babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
+    [
+      "@babel/preset-env",
+      { targets: { node: "current" }, useBuiltIns: "entry", corejs: 3 },
+    ],
+    "@babel/preset-typescript",
   ],
 };
